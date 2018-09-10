794b2338b564031cab29f13de09d2b6ecb4db400
mikemandelol <32073290+mikemandelol@users.noreply.github.com>
2018-09-10 09:50:27 -0400

    I forgot the the first 1 my bad

---
+++ sum.java
@@ -0,0 +1,15 @@
+import cs1.Keyboard;
+import java.util.Scanner;
+
+public class sum
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+
+		System.out.println(1+2+3+4+5+6+7+8+9+10);
+	}
+}
+
+
+
